50df3e55800dd71b9ec7b2c09b1c6cf35975ff4c
Davan@b4u <dvnreddy.v@gmail.com>
2025-01-29 10:20:09 +0100

    added new unit test case

--- math.test.js
+++ math.test.js
@@ -5,6 +5,14 @@ test('adds 2 + 3 to equal 5', () => {
     expect(add(2, 3)).toBe(5);
 });
 
+test('adds 3 + 3 to equal 6', () => {
+    expect(add(3, 3)).toBe(6);
+});
+
+test('adds 4 + 9 to equal 13', () => {
+    expect(add(4, 9)).toBe(13);
+});
+
 test('multiplies 2 * 3 to equal 6', () => {
     expect(multiply(2, 3)).toBe(6);
 });
