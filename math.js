bd71d815af2c4203d9789c4176af689b1f95cb3c
Davan@b4u <dvnreddy.v@gmail.com>
2025-01-29 10:12:53 +0100

    updated unittests

---
+++ math.js
@@ -0,0 +1,12 @@
+// math.js
+function add(a, b) {
+    console.log(a + b)
+    return a + b;
+}
+
+function multiply(a, b) {
+    console.log(a * b)
+    return a * b;
+}
+
+module.exports = { add, multiply };
